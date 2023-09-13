d4ce8824990fbb88d9de9e27bedada8f2822a77d
Nimai360 <nimai360@hotmail.com>
2023-09-13 17:23:56 -0300

    Finalizado, mas precisa...

--- SistemaGerenciamentoEstudantes.java
+++ SistemaGerenciamentoEstudantes.java
@@ -9,6 +9,8 @@ import view.Util;
 public class SistemaGerenciamentoEstudantes {
 	public static void main(String[] args) {
 		MensagemParaUsuario msgUsuario = new Console();
+		BancoDeDados db = new BancoDeDados(BancoDeDados.POSTGRE, "localhost", 5432, BancoDeDados.DB_NAME, "postgres",
+				"123456");
 
 		Integer op = msgUsuario.optionInfo("Escolha a forma de exibição:", "Modo de Exibição\n\n",
 				new String[] { "Console", "Interface" });
@@ -16,8 +18,6 @@ public class SistemaGerenciamentoEstudantes {
 		if (op == 2) {
 			msgUsuario = new OptionPane();
 		}
-		BancoDeDados db = new BancoDeDados(BancoDeDados.POSTGRE, "localhost", 5432, BancoDeDados.DB_NAME, "postgres",
-				"123456");
 
 		Menu m = new Menu(msgUsuario, db);
 		m.exibirMenu();
